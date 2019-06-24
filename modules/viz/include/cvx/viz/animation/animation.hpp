b84261be193211818f187b6214a2f53b6022f5e3
malasiot <malasiot@iti.gr>
2019-06-24 15:24:56 +0300

    animation

--- modules/viz/include/cvx/viz/animation/animation.hpp
+++ modules/viz/include/cvx/viz/animation/animation.hpp
@@ -3,34 +3,19 @@
 
 #include <memory>
 #include <vector>
+#include <limits>
+#include <cmath>
 
 namespace cvx { namespace viz {
-
-class TimeInterpolator {
-public:
-    virtual float value(float fraction) const = 0 ;
-};
-
-class LinearInterpolator: public TimeInterpolator {
-public:
-    float value(float fraction) const override {
-        return fraction ;
-    }
-};
-
-template <class T> class Evaluator {
-public:
-    Evaluator(TimeInterpolator *ti) ;
-    virtual T eval(float ts, KeyFrameSet<T> f) ;
-
-protected:
-    TimeInterpolator *ti_ ;
-};
 
 template<class T>
 class KeyFrame {
 public:
     KeyFrame(float t, T v): t_(t), value_(v) {}
+
+    float getTimeStamp() const { return t_ ; }
+    const T &getValue() const { return value_ ; }
+
     float t_ ;
     T value_ ;
 };
@@ -39,43 +24,156 @@ template<class T>
 class KeyFrameSet {
 public:
 
-    KeyFrameSet(T start, T stop) {
-        key_frames_.emplace_back(0.0f, start) ;
-        key_frames_.emplace_back(0.0f, stop) ;
-    }
 
     KeyFrameSet(std::initializer_list<KeyFrame<T>> init): key_frames_(init) {
         assert(key_frames_.size() > 1) ;
+        computeDuration() ;
+    }
+
+    float getDuration() const { duration_ ; }
+
+    int numKeyFrames() const { return key_frames_.size() ; }
+
+    const KeyFrame<T> &getKeyFrame(int idx) const { return key_frames_.get(idx) ; }
+
+private:
+
+    void computeDuration() {
+        duration_ = key_frames_.back().t_ ;
     }
 
     std::vector<KeyFrame<T>> key_frames_ ;
+    float duration_ ;
 };
 
-
-template <class T>
-class LinearScalarEvaluator: public Evaluator<T> {
+class EasingCurve {
 public:
-    T eval(float ts, KeyFrameSet<T> f) {
-        for (int i = 1; i < f.key_frames_.size(); ++i) {
-            const KeyFrame<T> &kf = f.key_frames_[i] ;
-            if ( ts < kf.t_ ) {
+    virtual float value(float fraction) const = 0 ;
+};
 
-            }
+class LinearEasingCurve: public EasingCurve {
+public:
+    float value(float fraction) const override {
+        return fraction ;
     }
 };
 
+template <class T> class KeyFrameInterpolator {
+    public:
+    static T interpolate(float t, KeyFrame<T> &kf1, const KeyFrame<T> &kf2) ;
+};
+
+template <class T> class LinearKeyFrameInterpolator {
+    public:
+    static T interpolate(float t, KeyFrame<T> &kf1, const KeyFrame<T> &kf2) {
+        return t * kf1.getValue() + ( 1.0f - t ) * kf2.getValue() ;
+    }
+};
+
+template <typename T>
+class KeyFrameSampler {
+    virtual T interpolate(const KeyFrameSet<T> &data, float fraction) = 0 ;
+};
+
+template <typename T, template<class S> class Interpolator = LinearKeyFrameInterpolator>
+class LinearKeyFrameSampler {
+public:
+
+    virtual T interpolate(const KeyFrameSet<T> &data, float fraction) override {
+
+        assert(data.size() > 2) ;
+
+        int numKeyFrames = data.numKeyFrames() ;
+        const auto &firstKeyFrame = data.getKeyFrame(0) ;
+        const auto &lastKeyFrame = data.getKeyFrame(numKeyFrames-1) ;
+
+        if ( fraction <= firstKeyFrame.getTimeStamp() ) { // extrapolate using the first two key frames
+            const auto &nextKeyFrame = data.getKeyFrame(1) ;
+
+            float intervalFraction = ( fraction - firstKeyFrame.getTimeStamp() ) /
+                      ( nextKeyFrame.getTimeStamp() - firstKeyFrame.getTimeStamp() );
+            return Interpolator<T>::interpolate(intervalFraction, firstKeyFrame.getValue(),  nextKeyFrame.getValue());
+        } else if ( fraction >= lastKeyFrame.getTimeStamp() ) {
+            const auto &prevKeyFrame = data.get(numKeyFrames - 2);
+
+            float intervalFraction = ( fraction - prevKeyFrame.getTimeStamp() ) /
+                      ( lastKeyFrame.getTimeStamp() - prevKeyFrame.getTimeStamp());
+            return Interpolator<T>::interpolate(intervalFraction, prevKeyFrame.getValue(),
+                          lastKeyFrame.getValue());
+        }
+
+        const auto & prevKeyFrame = firstKeyFrame ;
+
+        for (int i = 1; i < numKeyFrames; ++i ) {
+            const auto &nextKeyFrame = data.getKeyFrame(i);
+            if ( fraction < nextKeyFrame.getTimeStamp() ) {
+                 float intervalFraction = (fraction - prevKeyFrame.getTimeStamp()) /
+                          ( nextKeyFrame.getTimeStamp() - prevKeyFrame.getTimeStamp()) ;
+                 return Interpolator<T>::interpolate(intervalFraction, prevKeyFrame.getValue(),
+                              nextKeyFrame.getValue());
+            }
+            prevKeyFrame = nextKeyFrame;
+        }
+
+        // shouldn't reach here
+        return lastKeyFrame.getValue();
+
+    }
+
+};
 
 
-template <class T>
-class PropertyAnimation {
+// An abstract channel holds the value to be animated and updates its value its time the update function is called
+// The time value t should be between 0 and duration
+
+class AbstractChannel {
+public:
+    virtual float getDuration() const = 0 ;
+    virtual void update(float t) = 0 ;
+};
+
+// a standard channel over a specific type that intepolates key frames
+template <class S> class KeyFrameBasedChannel: public AbstractChannel {
+public:
+    KeyFrameBasedChannel(const KeyFrameSet<S> &f, const KeyFrameSampler<S> &sampler, const EasingCurve &ti) {
+        duration_ = f.getDuration() ;
+    }
+
+    virtual void update(float ts) override {
+        float tt = ti_.value(ts/duration_) ;
+        value_ = sampler_.interpolate(data_, tt * duration_) ;
+    } ;
+
+    float getDuration() const override { return duration_ ; }
+
+protected:
+
+    S value_ ;
+    float duration_ ;
+    const KeyFrameSet<S> &data_ ;
+    const KeyFrameSampler<S> &sampler_ ;
+    const EasingCurve &ti_ ;
+};
+
+
+// This should be overriden to return an api specific elapsed time in the same units as the animation data
+
+class AnimationTimer {
+public:
+    AnimationTimer() = default ;
+
+    virtual float getTime() const = 0 ;
+};
+
+
+class Animation {
 public:
     enum RepeatMode {  RESTART, REVERSE  } ;
+    enum State { RUNNING, PAUSED, STOPPED } ;
 
     // define an animation of specific duration for each repetition cycle
 
-    PropertyAnimation(T start, T stop, float duration): start_value_(start), stop_value_(stop), duration_(duration),
-        interpolator_(new LinearInterpolator()),
-        evaluator_(new LinearScalarEvaluator<T>() ){} ;
+    Animation(AnimationTimer *timer): timer_(timer) {}
 
     // set/get the number of repetitions
 
@@ -98,36 +196,79 @@ public:
         return repeat_mode_ ;
     }
 
-    // set/get the initial delay of the animation
-
-    void setStartDelay(float delay) {
-        start_delay_ = delay ;
+    void addChannel(const AbstractChannel *e) {
+        channels_.emplace_back(e) ;
+        cycle_duration_ = std::max(cycle_duration_, e->getDuration()) ;
     }
 
-    float getStartDelay() const {
-        return start_delay_ ;
+    void update() {
+
+        if ( state_ == RUNNING ) {
+            float elapsed = timer_->getTime() - start_time_ ;
+            bool done = false ;
+
+            float fraction = cycle_duration_ > 0 ? elapsed / cycle_duration_ : 1.f;
+            if ( fraction >= 1.f ) {
+                if ( cycle_ < repeat_count_ || repeat_count_ == -1 ) {
+
+                    if ( repeat_mode_ == REVERSE )
+                        reverse = !reverse ;
+
+                    cycle_ += (int)fraction ;
+                    fraction = fmod(fraction, 1.0f) ;
+
+                    start_time_ += cycle_duration_ ;
+                } else {
+                    done = true ;
+                    fraction = std::min(fraction, 1.0f);
+                }
+             }
+
+            if (  reverse )
+                fraction = 1.f - fraction;
+
+            for( AbstractChannel *channel: channels_ ) {
+                channel->update(fraction) ;
+            }
+
+            if ( done ) stop() ;
+        }
     }
 
-    void setTimeInterpolator(TimeInterpolator *i) {
-        interpolator_.reset(i) ;
+    void play() {
+        if ( state_ == PAUSED ) {
+            start_time_ = paused_time_ ;
+        } else if ( state_ == STOPPED ) {
+            start_time_ = timer_->getTime() ;
+        }
+        state_ = RUNNING ;
     }
 
-    void setEvaluator(Evaluator<T> *e) ;
+    void pause() {
+        if ( state_ != PAUSED ) {
+            paused_time_ = timer_->getTime() ;
+            state_ = PAUSED ;
+        }
+    }
+    void stop() {
+        if ( state_ != STOPPED ) {
+            state_ = STOPPED ;
+        }
+    }
 
-    // get the current value of the animated property
-    T getValue() const ;
 
 private:
-    float duration_ ;
-    float start_delay_ = 0.0f ;
+    AnimationTimer *timer_ ;
+    float cycle_duration_ = 0.f ;
+    float start_time_ = 0.f, paused_time_ = 0.0 ;
+    float cycle_time_ = 0.0f ;
+    bool reverse = false ;
+    State state_ = STOPPED ;
     int repeat_count_ = -1 ; // infinite
     RepeatMode repeat_mode_ = REVERSE ; // repeat mode
     float fraction_ = 0; // the percentage of the animation [0, 1] within the current cycle
     int cycle_ = 0; // the current cycle ;
-
-    std::unique_ptr<TimeInterpolator> interpolator_ ;
-    std::unique_ptr<Evaluator<T>> evaluator_ ;
-    T start_value_, stop_value_ ;
+    std::vector<AbstractChannel *> channels_ ;
 };
 
 
