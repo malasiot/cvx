a04b8aa61d1dde8aca56ab84a581b5b49f628fba
malasiot <malasiot@iti.gr>
2020-08-21 09:42:43 +0300

    fix

---
+++ modules/viz/include/cvx/viz/physics/constraints.hpp
@@ -0,0 +1,27 @@
+#ifndef CVX_VIZ_PHYSICS_CONSTRAINTS_HPP
+#define CVX_VIZ_PHYSICS_CONSTRAINTS_HPP
+
+#include <bullet/BulletDynamics/btBulletDynamicsCommon.h>
+
+#include <cvx/viz/physics/rigid_body.hpp>
+
+namespace cvx { namespace viz {
+
+class Constraint {
+public:
+    btTypedConstraint *handle() const { return handle_ ; }
+protected:
+
+    btTypedConstraint *handle_ ;
+
+};
+class Point2PointConstraint: public Constraint {
+
+public:
+    Point2PointConstraint(const RigidBody &b1, const RigidBody &b2, const Eigen::Vector3f &pivot1, const Eigen::Vector3f &pivot2) ;
+};
+
+} // namespace viz
+} // namespace cvx
+
+#endif
---
+++ modules/viz/src/physics/constraints.cpp
@@ -0,0 +1,10 @@
+#include <cvx/viz/physics/constraints.hpp>
+#include <cvx/viz/physics/convert.hpp>
+
+namespace cvx { namespace viz {
+
+Point2PointConstraint::Point2PointConstraint(const cvx::viz::RigidBody &b1, const cvx::viz::RigidBody &b2, const Eigen::Vector3f &pivot1, const Eigen::Vector3f &pivot2) {
+    handle_ = new btPoint2PointConstraint (*b1.handle(), *b2.handle(), eigenVectorToBullet(pivot1), eigenVectorToBullet(pivot2)) ;
+}
+
+}}
